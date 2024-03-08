20842bd672f4f36b10f1309163b041f6635b99db
Elena Koltyrina <geek.albis@gmail.com>
2024-03-08 17:36:56 +0100

    добавленны 3 функции и 3 теста, распаковка, сортировка, дата в формате ДД.ММ.ГГГГ

--- src/functions.py
+++ src/functions.py
@@ -1,11 +1,40 @@
 import json
+import pprint
+from config import ROOT_DIR
+import os
+import datetime
+pp = pprint.PrettyPrinter(indent=7)
 
+operation_path = os.path.join(ROOT_DIR, "src", 'operations.json')
 
-def load_data():
-    """распаковка файла json"""
-    with open('operations.json', 'r') as file:
+def load_data(operation_path):
+    """функция распаковки файла json"""
+    with open(operation_path, 'r') as file:
         file = json.loads(file.read())
     return file
 
+#pp.pprint(load_data(operation_path))
 
+
+
+def sorting_executed(file):
+    """сортировка выполненных операций"""
+    new_list = []
+    for element in file:
+        if element and element["state"] == "EXECUTED":
+            new_list.append(element)
+    return new_list
+
+new_list = sorting_executed(load_data(operation_path))
+#pp.pprint(sorting_executed(load_data(operation_path)))
+
+def sorting_data_time(new_list):
+    """Функция сортирует по дате и выводит дату в формате ДД.ММ.ГГГГ"""
+    new_list.sort(key=lambda x: x.get("date"), reverse=True)
+    for element in new_list:
+        element["date"] = datetime.datetime.strptime(element["date"], '%Y-%m-%dT%H:%M:%S.%f').strftime('%d.%m.%Y')
+    return new_list
+
+
+#pp.pprint(sorting_data_time(new_list))
 
---
+++ tests/test_functions.py
@@ -0,0 +1,35 @@
+from src.functions import load_data, sorting_executed, sorting_data_time
+from config import ROOT_DIR
+import os
+
+
+def test_load_data():
+    """Проверка работоспособности распаковки файла json"""
+    operation_path = os.path.join(ROOT_DIR, "src", 'operations.json')
+    assert type(load_data(operation_path) == list and len(load_data(operation_path)) > 0)
+    assert type(load_data(operation_path)[0]) == dict
+    assert type(load_data(operation_path)[0]['state']) == str
+    assert type(load_data(operation_path)[34]['id']) == int
+
+
+def test_sorting_executed():
+    """Проверка работоспособности сортировки по ключы state выполенной операций"""
+    data = [{"id": 1, "state": "EXECUTED"}, {"id": 2, "state": "CANCELED"}, {"id": 3, "state": "EXECUTED"}]
+    expected = [{"id": 1, "state": "EXECUTED"}, {"id": 3, "state": "EXECUTED"}]
+    assert sorting_executed(data) == expected
+
+
+def test_sorting_data_time():
+    """Проверка сортировки по дате"""
+    data = [
+        {"id": 1, 'date': '2022-01-13T13:00:58.458625'},
+        {"id": 2, 'date': '2024-01-23T01:48:30.477053'},
+        {"id": 3, 'date': '2023-01-21T01:10:28.317704'}
+    ]
+    expected = [
+        {"id": 2, 'date': '23.01.2024'},
+        {"id": 3, 'date': '21.01.2023'},
+        {"id": 1, 'date': '13.01.2022'}
+    ]
+    assert sorting_data_time(data) == expected
+
