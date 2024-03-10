3ee1187ccc1ac7f412f0eef4b8e46a82c70562b3
Elena Koltyrina <geek.albis@gmail.com>
2024-03-10 14:45:31 +0100

    добавление маски к ключу "to"

--- src/functions.py
+++ src/functions.py
@@ -35,12 +35,42 @@ def sorting_data_time(validated):
         element["date"] = datetime.datetime.strptime(element["date"], '%Y-%m-%dT%H:%M:%S.%f').strftime('%d.%m.%Y')
     return validated
 
-#print(sorting_data_time(new_list))
+
 #pp.pprint(sorting_data_time(validated))
 def last_five_actions(validated):
     """Функция возвращает последние 5 операций и их описание"""
-    return validated[:5]
+    return validated[:30]
 
-pp.pprint(last_five_actions(sorting_data_time(validated)))
+ss =  last_five_actions(sorting_data_time(validated))
+#pp.pprint(ss)
+
+def masks_the_result(ss):
+    """Функция маскирует результат"""
+    for index in ss:
+        if 'Счет' in index["to"] :
+            index["to"] = index["to"][:5] + "**" + index["to"][-4:]
+        else:
+            index["to"] = index["to"][0:-12] + " " + index["to"][-13:-11] + "**" + " " + "****" + " " + index["to"][-5:-1]
+    return ss
+#Visa Platinum 7000 79** **** 6361
+
+nn = masks_the_result(ss)
+#pp.pprint(nn)
+
+def formatted_information(nn):
+    """Функция вывода информации в читаемом виде"""
+    for index in nn:
+        if index.get('from') is not None:
+            print(f"{index['date']} {index['description']}\n"
+                  f"{index.get('from')} -> {index['to']}\n"
+                  f"{index['operationAmount']['amount']} {index['operationAmount']['currency']['name']}\n")
+        if index.get('from') is None:
+            print(f"{index['date']} {index['description']}\n"
+                  f"{index['to']}\n"
+                  f"{index['operationAmount']['amount']} {index['operationAmount']['currency']['name']}\n")
+
+formatted_information(nn)
+
+
 
 
